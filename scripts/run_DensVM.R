440f6f4a5495f94f6c868bf3fccd20d2ce65dde4
Lukas Weber <lukmweber@gmail.com>
2016-07-21 15:59:22 +0200

    updated version of cytofkit/DensVM

--- scripts/run_DensVM.R
+++ scripts/run_DensVM.R
@@ -1,7 +1,7 @@
 #########################################################################################
 # R script to run DensVM (cytofkit R/Bioconductor package)
 #
-# Lukas M. Weber, March 2016
+# Lukas Weber, July 2016
 #########################################################################################
 
 
@@ -10,77 +10,98 @@ library(cytofkit)
 
 
 
+
 #################
 ### LOAD DATA ###
 #################
 
-# working directory (need to reset after running DensVM)
-
-INIT_DIR <- getwd()
-
-# use non-transformed data files, since DensVM will transform automatically
+# filenames
 
 DATA_DIR <- "../../benchmark_data_sets"
 
-file_Levine_32 <- file.path(DATA_DIR, "Levine_2015_marrow_32/data/Levine_2015_marrow_32_notransform.fcs")
-file_Levine_13 <- file.path(DATA_DIR, "Levine_2015_marrow_13/data/Levine_2015_marrow_13_notransform.fcs")
-file_Nilsson <- file.path(DATA_DIR, "Nilsson_2013_HSC/data/Nilsson_2013_HSC_notransform.fcs")
-file_Mosmann <- file.path(DATA_DIR, "Mosmann_2014_activ/data/Mosmann_2014_activ_notransform.fcs")
+files <- list(
+  Levine_32dim = file.path(DATA_DIR, "Levine_32dim/data/Levine_32dim.fcs"), 
+  Levine_13dim = file.path(DATA_DIR, "Levine_13dim/data/Levine_13dim.fcs"), 
+  Samusik_01   = file.path(DATA_DIR, "Samusik/data/Samusik_01.fcs"), 
+  Samusik_all  = file.path(DATA_DIR, "Samusik/data/Samusik_all.fcs"), 
+  Nilsson_rare = file.path(DATA_DIR, "Nilsson_rare/data/Nilsson_rare.fcs"), 
+  Mosmann_rare = file.path(DATA_DIR, "Mosmann_rare/data/Mosmann_rare.fcs"), 
+  FlowCAP_ND   = file.path(DATA_DIR, "FlowCAP_ND/data/FlowCAP_ND.fcs"), 
+  FlowCAP_WNV  = file.path(DATA_DIR, "FlowCAP_WNV/data/FlowCAP_WNV.fcs")
+)
 
-data_Levine_32 <- flowCore::read.FCS(file_Levine_32, transformation = FALSE)
-data_Levine_13 <- flowCore::read.FCS(file_Levine_13, transformation = FALSE)
-data_Nilsson <- flowCore::read.FCS(file_Nilsson, transformation = FALSE)
-data_Mosmann <- flowCore::read.FCS(file_Mosmann, transformation = FALSE)
+# FlowCAP data sets are treated separately since they require clustering algorithms to be
+# run individually for each sample
 
-head(data_Levine_32)
-head(data_Levine_13)
-head(data_Nilsson)
-head(data_Mosmann)
-
-dim(data_Levine_32)
-dim(data_Levine_13)
-dim(data_Nilsson)
-dim(data_Mosmann)
+is_FlowCAP <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, TRUE)
 
 
-# note DensVM also requires a copy of the input FCS file in the output directory
+# load data files
 
-out_dir_Levine_32 <- "../results_auto/DensVM/Levine_2015_marrow_32"
-out_dir_Levine_13 <- "../results_auto/DensVM/Levine_2015_marrow_13"
-out_dir_Nilsson <- "../results_auto/DensVM/Nilsson_2013_HSC"
-out_dir_Mosmann <- "../results_auto/DensVM/Mosmann_2014_activ"
+data <- vector("list", length(files))
+names(data) <- names(files)
+
+for (i in 1:length(data)) {
+  f <- files[[i]]
+  
+  if (!is_FlowCAP[i]) {
+    data[[i]] <- flowCore::exprs(flowCore::read.FCS(f, transformation = FALSE, truncate_max_range = FALSE))
+    
+  } else {
+    smp <- flowCore::exprs(flowCore::read.FCS(f, transformation = FALSE, truncate_max_range = FALSE))
+    smp <- smp[, "sample"]
+    d <- flowCore::read.FCS(f, transformation = FALSE, truncate_max_range = FALSE)
+    d <- flowCore::split(d, smp)
+    data[[i]] <- lapply(d, function(s) flowCore::exprs(s))
+  }
+}
+
+head(data[[1]])
+head(data[[8]][[1]])
+
+sapply(data, length)
+
+sapply(data[!is_FlowCAP], dim)
+sapply(data[is_FlowCAP], function(d) {
+  sapply(d, function(d2) {
+    dim(d2)
+  })
+})
 
 
 # indices of protein marker columns
 
-marker_cols_Levine_32 <- 5:36
-marker_cols_Levine_13 <- 1:13
-marker_cols_Nilsson <- c(5:7, 9:18)
-marker_cols_Mosmann <- c(7:9, 11:21)
+marker_cols <- list(
+  Levine_32dim = 5:36, 
+  Levine_13dim = 1:13, 
+  Samusik_01   = 9:47, 
+  Samusik_all  = 9:47, 
+  Nilsson_rare = c(5:7, 9:18), 
+  Mosmann_rare = c(7:9, 11:21), 
+  FlowCAP_ND   = 3:12, 
+  FlowCAP_WNV  = 3:8
+)
+sapply(marker_cols, length)
 
-length(marker_cols_Levine_32)
-length(marker_cols_Levine_13)
-length(marker_cols_Nilsson)
-length(marker_cols_Mosmann)
 
-# select parameters (protein names) for DensVM
+# subset data: protein marker columns only
 
-para_Levine_32 <- colnames(data_Levine_32)[marker_cols_Levine_32]
-para_Levine_13 <- colnames(data_Levine_13)[marker_cols_Levine_13]
-para_Nilsson <- colnames(data_Nilsson)[marker_cols_Nilsson]
-para_Mosmann <- colnames(data_Mosmann)[marker_cols_Mosmann]
+for (i in 1:length(data)) {
+  if (!is_FlowCAP[i]) {
+    data[[i]] <- data[[i]][, marker_cols[[i]]]
+  } else {
+    for (j in 1:length(data[[i]])) {
+      data[[i]][[j]] <- data[[i]][[j]][, marker_cols[[i]]]
+    }
+  }
+}
 
-length(para_Levine_32)
-length(para_Levine_13)
-length(para_Nilsson)
-length(para_Mosmann)
-
-# number of points to subsample
-
-n_sub_Levine_32 <- 20000
-n_sub_Levine_13 <- 20000
-n_sub_Nilsson <- 20000
-n_sub_Mosmann <- 20000
+sapply(data[!is_FlowCAP], dim)
+sapply(data[is_FlowCAP], function(d) {
+  sapply(d, function(d2) {
+    dim(d2)
+  })
+})
 
 
 
@@ -89,72 +110,109 @@ n_sub_Mosmann <- 20000
 ### Run DensVM: automatic number of clusters ###
 ################################################
 
+# number of points to subsample
+
+n_sub <- 20000
+
+
 # run DensVM with automatic selection of number of clusters (manual selection is not available)
 
-# use main function "cytof_tsne_densvm"
-# graphical version can also be launched with "cytof_tsne_densvm_GUI()"
+# use main functions: cytof_dimReduction(), cytof_cluster()
 
-# results will be saved to files in the output directories
+seed <- 123
+out <- runtimes <- vector("list", length(data))
+names(out) <- names(runtimes) <- names(data)
 
-set.seed(123)
-runtime_Levine_32 <- system.time({
-  cytofkit::cytof_tsne_densvm(fcsFile = file_Levine_32, resDir = out_dir_Levine_32, 
-                              lgclMethod = "auto", para = para_Levine_32, 
-                              fixedNum = n_sub_Levine_32, verbose = TRUE)
-})
-setwd(INIT_DIR)  # reset working directory
+for (i in 1:length(data)) {
+  
+  if (!is_FlowCAP[i]) {
+    set.seed(seed)
+    runtimes[[i]] <- system.time({
+      # subsampling (could also use function cytof_exprsMerge())
+      x <- data[[i]][sample(1:n_sub, n_sub), ]
+      
+      y <- cytof_dimReduction(x, method = "tsne")
+      out[[i]] <- cytof_cluster(y, x, method = "DensVM")
+    })
+    cat("data set", names(data[i]), ": run complete\n")
+    
+  } else {
+    # FlowCAP data sets: run clustering algorithm separately for each sample
+    out[[i]] <- runtimes[[i]] <- vector("list", length(data[[i]]))
+    names(out[[i]]) <- names(runtimes[[i]]) <- names(data[[i]])
+    
+    for (j in 1:length(data[[i]])) {
+      set.seed(seed)
+      runtimes[[i]][[j]] <- system.time({
+        # subsampling (could also use function cytof_exprsMerge())
+        x <- data[[i]][[j]][sample(1:n_sub, n_sub), ]
+        
+        y <- cytof_dimReduction(x, method = "tsne")
+        out[[i]][[j]] <- cytof_cluster(y, x, method = "DensVM")
+      })
+    }
+    cat("data set", names(data[i]), ": run complete\n")
+    
+    # FlowCAP data sets: sum runtimes over samples
+    runtimes_i <- do.call(rbind, runtimes[[i]])[, 1:3]
+    runtimes_i <- colSums(runtimes_i)
+    names(runtimes_i) <- c("user", "system", "elapsed")
+    runtimes[[i]] <- runtimes_i
+  }
+}
 
+# extract cluster labels
+clus <- vector("list", length(data))
+names(clus) <- names(data)
 
-set.seed(123)
-runtime_Levine_13 <- system.time({
-  cytofkit::cytof_tsne_densvm(fcsFile = file_Levine_13, resDir = out_dir_Levine_13, 
-                              lgclMethod = "auto", para = para_Levine_13, 
-                              fixedNum = n_sub_Levine_13, verbose = TRUE)
-})
-setwd(INIT_DIR)  # reset working directory
+for (i in 1:length(clus)) {
+  if (!is_FlowCAP[i]) {
+    clus[[i]] <- as.numeric(out[[i]])
+    
+  } else {
+    # FlowCAP data sets
+    clus_list_i <- vector("list", length(data[[i]]))
+    for (j in 1:length(data[[i]])) {
+      clus_list_i[[j]] <- as.numeric(out[[i]][[j]])
+    }
+    
+    # convert FlowCAP cluster labels into format "sample_number"_"cluster_number"
+    # e.g. sample 1, cluster 3 -> cluster label 1_3
+    names_i <- rep(names(clus_list_i), times = sapply(clus_list_i, length))
+    clus_collapse_i <- unlist(clus_list_i, use.names = FALSE)
+    clus[[i]] <- paste(names_i, clus_collapse_i, sep = "_")
+  }
+}
 
+sapply(clus, length)
 
-set.seed(123)
-runtime_Nilsson <- system.time({
-  cytofkit::cytof_tsne_densvm(fcsFile = file_Nilsson, resDir = out_dir_Nilsson, 
-                              lgclMethod = "auto", para = para_Nilsson, 
-                              fixedNum = n_sub_Nilsson, verbose = TRUE)
-})
-setwd(INIT_DIR)  # reset working directory
+# cluster sizes and number of clusters
+# (for FlowCAP data sets, total no. of clusters = no. samples * no. clusters per sample)
+table(clus[[1]])
+sapply(clus, function(cl) length(table(cl)))
 
+# save cluster labels
+files_labels <- paste0("../results_auto/DensVM/DensVM_labels_", 
+                       names(clus), ".txt")
 
-set.seed(123)
-runtime_Mosmann <- system.time({
-  cytofkit::cytof_tsne_densvm(fcsFile = file_Mosmann, resDir = out_dir_Mosmann, 
-                              lgclMethod = "auto", para = para_Mosmann, 
-                              fixedNum = n_sub_Mosmann, verbose = TRUE)
-})
-setwd(INIT_DIR)  # reset working directory
+for (i in 1:length(files_labels)) {
+  res_i <- data.frame(label = clus[[i]])
+  write.table(res_i, file = files_labels[i], row.names = FALSE, quote = FALSE, sep = "\t")
+}
 
+# save runtimes
+runtimes <- lapply(runtimes, function(r) r["elapsed"])
+runtimes <- t(as.data.frame(runtimes, row.names = "runtime"))
 
-# save runtime
-
-runtime_DensVM <- t(data.frame(
-  Levine_2015_marrow_32 = runtime_Levine_32["elapsed"], 
-  Levine_2015_marrow_13 = runtime_Levine_13["elapsed"], 
-  Nilsson_2013_HSC = runtime_Nilsson["elapsed"], 
-  Mosmann_2014_activ = runtime_Mosmann["elapsed"], 
-  row.names = "runtime"))
-
-write.table(runtime_DensVM, 
-            file = "../results_auto/runtime/runtime_DensVM.txt", 
+write.table(runtimes, file = "../results_auto/runtimes/runtime_DensVM.txt", 
             quote = FALSE, sep = "\t")
 
-
 # save session information
-
 sink(file = "../results_auto/session_info/session_info_DensVM.txt")
-sessionInfo()
+print(sessionInfo())
 sink()
 
+cat("DensVM automatic : all runs complete\n")
 
-# save R objects
-
-save.image(file = "../results_auto/RData_files/results_DensVM.RData")
 
 
