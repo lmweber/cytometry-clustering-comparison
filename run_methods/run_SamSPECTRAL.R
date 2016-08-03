09186770d2dc46fa0a5142a34dfb80673e58f4f9
Lukas Weber <lukmweber@gmail.com>
2016-08-03 11:13:57 +0200

    update SamSPECTRAL: auto only

--- run_methods/run_SamSPECTRAL.R
+++ run_methods/run_SamSPECTRAL.R
@@ -1,7 +1,7 @@
 #########################################################################################
 # R script to run SamSPECTRAL
 #
-# Lukas Weber, July 2016
+# Lukas Weber, August 2016
 #########################################################################################
 
 
@@ -76,16 +76,12 @@ n_sub <- 100000
 
 for (i in ix_subsample) {
   if (!is_FlowCAP[i]) {
-    set.seed(123)
+    set.seed(1234)
     data[[i]] <- data[[i]][sample(1:nrow(data[[i]]), n_sub), ]
     # save subsampled population IDs
     true_labels_i <- data[[i]][, "label", drop = FALSE]
-    files_true_labels_i <- paste0(c("../../results_auto/SamSPECTRAL/true_labels_SamSPECTRAL_", 
-                                    "../../results_manual/SamSPECTRAL/true_labels_SamSPECTRAL_"), 
-                                  names(data)[i], ".txt")
-    for (f in files_true_labels_i) {
-      write.table(true_labels_i, file = f, row.names = FALSE, quote = FALSE, sep = "\t")
-    }
+    file_true_labels_i <- paste0("../../results_auto/SamSPECTRAL/true_labels_SamSPECTRAL_", names(data)[i], ".txt")
+    write.table(true_labels_i, file = file_true_labels_i, row.names = FALSE, quote = FALSE, sep = "\t")
   }
 }
 
@@ -134,7 +130,7 @@ sapply(data[is_FlowCAP], function(d) {
 # run SamSPECTRAL with selection of number of clusters via arguments normal.sigma and separation.factor
 # (values from Levine et al. 2015)
 
-seed <- 123
+seed <- 1234
 out <- runtimes <- vector("list", length(data))
 names(out) <- names(runtimes) <- names(data)
 
@@ -221,117 +217,6 @@ print(sessionInfo())
 sink()
 
 cat("SamSPECTRAL automatic : all runs complete\n")
-
-
-
-
-#############################################################
-### Run SamSPECTRAL: manually selected number of clusters ###
-#############################################################
-
-# run SamSPECTRAL with manual selection of number of clusters
-
-# number of clusters k
-k <- list(
-  Levine_32dim = 40, 
-  Levine_13dim = 40, 
-  Samusik_01   = 40, 
-  Samusik_all  = 40, 
-  Nilsson_rare = 40, 
-  Mosmann_rare = 40, 
-  FlowCAP_ND   = 7, 
-  FlowCAP_WNV  = 4
-)
-
-seed <- 123
-out <- runtimes <- vector("list", length(data))
-names(out) <- names(runtimes) <- names(data)
-
-for (i in 1:length(data)) {
-  
-  if (!is_FlowCAP[i]) {
-    set.seed(seed)
-    runtimes[[i]] <- system.time({
-      out[[i]] <- SamSPECTRAL(data[[i]], normal.sigma = 100, separation.factor = 1, 
-                              number.of.clusters = k[[i]])
-    })
-    cat("data set", names(data[i]), ": run complete\n")
-    
-  } else {
-    # FlowCAP data sets: run clustering algorithm separately for each sample
-    out[[i]] <- runtimes[[i]] <- vector("list", length(data[[i]]))
-    names(out[[i]]) <- names(runtimes[[i]]) <- names(data[[i]])
-    
-    for (j in 1:length(data[[i]])) {
-      set.seed(seed)
-      runtimes[[i]][[j]] <- system.time({
-        out[[i]][[j]] <- SamSPECTRAL(data[[i]][[j]], normal.sigma = 100, separation.factor = 1, 
-                                     number.of.clusters = k[[i]])
-      })
-    }
-    cat("data set", names(data[i]), ": run complete\n")
-    
-    # FlowCAP data sets: sum runtimes over samples
-    runtimes_i <- do.call(rbind, runtimes[[i]])[, 1:3]
-    runtimes_i <- colSums(runtimes_i)
-    names(runtimes_i) <- c("user", "system", "elapsed")
-    runtimes[[i]] <- runtimes_i
-  }
-}
-
-# extract cluster labels
-clus <- vector("list", length(data))
-names(clus) <- names(data)
-
-for (i in 1:length(clus)) {
-  if (!is_FlowCAP[i]) {
-    clus[[i]] <- out[[i]]
-    
-  } else {
-    # FlowCAP data sets
-    clus_list_i <- vector("list", length(data[[i]]))
-    names(clus_list_i) <- names(data[[i]])
-    for (j in 1:length(data[[i]])) {
-      clus_list_i[[j]] <- out[[i]][[j]]
-    }
-    
-    # convert FlowCAP cluster labels into format "sample_number"_"cluster_number"
-    # e.g. sample 1, cluster 3 -> cluster label 1_3
-    names_i <- rep(names(clus_list_i), times = sapply(clus_list_i, length))
-    clus_collapse_i <- unlist(clus_list_i, use.names = FALSE)
-    clus[[i]] <- paste(names_i, clus_collapse_i, sep = "_")
-  }
-}
-
-sapply(clus, length)
-
-# cluster sizes and number of clusters
-# (for FlowCAP data sets, total no. of clusters = no. samples * no. clusters per sample)
-table(clus[[1]])
-sapply(clus, function(cl) length(table(cl)))
-
-# save cluster labels
-files_labels <- paste0("../../results_manual/SamSPECTRAL/SamSPECTRAL_labels_", 
-                       names(clus), ".txt")
-
-for (i in 1:length(files_labels)) {
-  res_i <- data.frame(label = clus[[i]])
-  write.table(res_i, file = files_labels[i], row.names = FALSE, quote = FALSE, sep = "\t")
-}
-
-# save runtimes
-runtimes <- lapply(runtimes, function(r) r["elapsed"])
-runtimes <- t(as.data.frame(runtimes, row.names = "runtime"))
-
-write.table(runtimes, file = "../../results_manual/runtimes/runtime_SamSPECTRAL.txt", 
-            quote = FALSE, sep = "\t")
-
-# save session information
-sink(file = "../../results_manual/session_info/session_info_SamSPECTRAL.txt")
-print(sessionInfo())
-sink()
-
-cat("SamSPECTRAL manual : all runs complete\n")
 
 
 
