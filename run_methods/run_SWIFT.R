6fc7cd30d294fecd5399bea8e72c8e4435f18f70
Lukas Weber <lukmweber@gmail.com>
2016-08-11 11:18:38 +0200

    notes to run SWIFT

--- run_methods/run_SWIFT.R
+++ run_methods/run_SWIFT.R
@@ -4,10 +4,13 @@
 
 # Requires: Matlab, Statistics Toolbox, Parallel Computing Toolbox
 
-# 1. Copy FCS data files into the directory SWIFT/D/T. Note that SWIFT will automatically
-# perform an arcsinh transform, so the FCS files should not be transformed already.
-# 2. Open SWIFT GUI from Matlab by typing "swift_main" in command window (from within the
-# SWIFT program directory).
-# 3. Set parameters and select markers.
-# 4. Click on "Click to Run SWIFT".
-# 5. Output files are saved in the same directory SWIFT/D/T.
+# 1. Copy FCS file into a temporary directory, since results files will also be saved 
+# here.
+# 2. Run SWIFT graphical interface from Matlab by typing "swift_main" in command window.
+# 3. After graphical interface opens, select FCS file to import data. Note that SWIFT
+# will automatically perform an arcsinh transform, so the FCS file should not be
+# transformed already.
+# 4. Enter parameters and click to continue.
+# 5. After SWIFT completes, cluster labels will be saved in the file
+# "<original_filename>.Cluster_Output.txt" in the input directory. Cluster labels are in 
+# the "MergeCluster" column.
