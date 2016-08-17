e65c1e8d8703a96cd8bbe2c8c8d45c8cc01a0818
Lukas Weber <lukmweber@gmail.com>
2016-08-17 17:25:24 +0200

    update ACCENSE notes

--- run_methods/run_ACCENSE.R
+++ run_methods/run_ACCENSE.R
@@ -4,13 +4,11 @@
 
 # Requires: Standalone ACCENSE application
 
-# 1. Run ACCENSE application
-# 2. Select "Dimension reduction" under "Functions to run", set parameters, select
-# columns of markers, and click on "Run".
-# 3. When dimensionality reduction is complete, proceed to the clustering step. Note that
-# the output file "accense_output.csv" contains the dimensionality reduction results, and
-# can be loaded to continue directly from the clustering step.
-# 4. Inspect the t-SNE projection visually.
-# 5. To perform clustering, select "Classification and colouring", select either k-means 
-# or DBSCAN, and click "Calculate". After the clustering is complete, the output file 
-# "accense_output.csv" will be updated to include the cluster labels.
+# 1. Run ACCENSE application.
+# 2. Select "Dimension reduction" under "Functions to run", set parameters, import data,
+# select columns of markers, and click on "Run".
+# 3. After dimensionality reduction is complete, proceed to the clustering step and
+# visually check the t-SNE projection.
+# 4. To perform clustering, select "Classification and colouring", and choose either 
+# k-means, DBSCAN, or manual clustering. After clustering is complete, the output file
+# "accense_output.csv" will be automatically updated to include the cluster labels.
