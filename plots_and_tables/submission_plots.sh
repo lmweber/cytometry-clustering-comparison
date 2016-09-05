b32e5fe1210caa9dd4787e9821a192f7f75627d5
Lukas Weber <lukmweber@gmail.com>
2016-09-05 14:31:27 +0200

    generate EPS plots for submission

--- plots_and_tables/submission_plots.sh
+++ plots_and_tables/submission_plots.sh
@@ -5,15 +5,30 @@
 #########################################################################################
 
 
-# for journal submission: keep figures in PDF format, but rename to "Fig1.pdf" etc
+# for journal submission: convert to EPS format, and rename to "Fig1.eps" etc
 
 
 # Figure 1: Levine_32dim main results
+
+# rename and move PDF file
 cp ../../plots/Levine_32dim/main_plots/plots_multi_panel_Levine_32dim.pdf ../../plots_submission/Fig1_Levine_32dim.pdf
+# convert to EPS
+pdftops -eps -paperw 2100 ../../plots_submission/Fig1_Levine_32dim.pdf ../../plots_submission/Fig1_Levine_32dim.eps
+
 
 # Figure 2: Cluster medians (Levine_32dim, FlowSOM)
+
+# rename and move PDF file
 cp ../../plots/Levine_32dim/cluster_medians/cluster_medians_heatmap_FlowSOM_Levine32dim.pdf ../../plots_submission/Fig2_cluster_medians.pdf
+# convert to EPS
+pdftops -eps -paperw 1400 ../../plots_submission/Fig2_cluster_medians.pdf ../../plots_submission/Fig2_cluster_medians.eps
+
 
 # Figure 3: Mosmann_rare main results
+
+# rename and move PDF file
 cp ../../plots/Mosmann_rare/main_plots/plots_multi_panel_Mosmann_rare.pdf ../../plots_submission/Fig3_Mosmann_rare.pdf
+# convert to EPS
+pdftops -eps -paperw 970 ../../plots_submission/Fig3_Mosmann_rare.pdf ../../plots_submission/Fig3_Mosmann_rare.eps
+
 
